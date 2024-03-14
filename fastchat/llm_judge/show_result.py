93abc9a7fe186956bf29cbc02d0e2e149a4abb3d
a.zemerov <zemerovanton@gmail.com>
2024-03-14 17:30:37 +0800

    Show the whole pandas dataframe

--- fastchat/llm_judge/show_result.py
+++ fastchat/llm_judge/show_result.py
@@ -24,16 +24,16 @@ def display_result_single(args):
 
     print("\n########## First turn ##########")
     df_1 = df[df["turn"] == 1].groupby(["model", "turn"]).mean()
-    print(df_1.sort_values(by="score", ascending=False))
+    print(df_1.sort_values(by="score", ascending=False).to_string())
 
     if args.bench_name == "mt_bench":
         print("\n########## Second turn ##########")
         df_2 = df[df["turn"] == 2].groupby(["model", "turn"]).mean()
-        print(df_2.sort_values(by="score", ascending=False))
+        print(df_2.sort_values(by="score", ascending=False).to_string())
 
         print("\n########## Average ##########")
         df_3 = df[["model", "score"]].groupby(["model"]).mean()
-        print(df_3.sort_values(by="score", ascending=False))
+        print(df_3.sort_values(by="score", ascending=False).to_string())
 
 
 def display_result_pairwise(args):
@@ -89,7 +89,7 @@ def display_result_pairwise(args):
     )
     # print(df.sort_values(by="win_rate", ascending=False))
     # print(df.sort_values(by="loss_rate", ascending=True))
-    print(df.sort_values(by="win_rate_adjusted", ascending=False))
+    print(df.sort_values(by="win_rate_adjusted", ascending=False).to_string())
 
 
 if __name__ == "__main__":
